ca23e7bba073e42aae8c7ae7296668b3d7514a54
heungseok <heungseok2@gmail.com>
2017-05-13 23:50:13 +0900

    init npm and install exress module

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 /**
  * Created by heungseok2 on 2017-05-13.
  */
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+// var world = require('./js/server_world');
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/data', express.static(__dirname + '/data'));
+app.use('/img', express.static(__dirname + '/img'));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(4444, function(){
+    console.log("Server Running and Listen to port 4444");
+});
